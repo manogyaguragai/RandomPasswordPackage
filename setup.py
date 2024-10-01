bb1b6a95d04e9d7683bfb76106a8ddf87720c071
Manogya Guragai <manogyaguragai24@gmail.com>
2024-10-01 22:32:04 +0545

    name change

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='RamdomPasswordGenPKG', 
+    name='random-password-generator', 
     version='0.1',  
     description='A simple customizable random password generator package.',  
     long_description=open('README.md').read(), 
